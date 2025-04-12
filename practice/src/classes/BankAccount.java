98def6213073b95fe2f8ca756386011eeda48565
his-freeman <ishan0414@naver.com>
2025-04-12 15:31:06 +0900

    클래스 참조

--- practice/src/classes/BankAccount.java
+++ practice/src/classes/BankAccount.java
@@ -5,10 +5,32 @@ package classes;
 public class BankAccount {
 	private String ano;  //계좌 번호
 	private String owner; //계좌주
-	private int balace;   //잔고
+	private int balance;   //잔고
 	
 	//기본 생성자
 	public BankAccount() {}
 	
-
+	//매개변수가 있는 생성자
+	public BankAccount(String ano, String owner, int balance) {
+		this.ano = ano;
+		this.owner = owner;
+		this.balance = balance;
+	}
+	
+	//설정자(setter) - set+멤버이름
+	void setAno(String ano) {this.ano = ano;}
+	void setOwner(String owner) {this.owner = owner;}
+	void setBalance(int balance) {this.balance = balance;}
+	
+	//접근자(getter)
+	String getAno() {return ano;}
+	String getOwner() {return owner;}
+	int getBalance() {return balance;}
+	
+	//계좌 정보를 출력하는 메서드
+	void displayInfo() {
+		System.out.println("게좌번호: " + ano);
+		System.out.println("게좌주: " + owner);
+		System.out.println("잔고: " + balance);
+	}
 }
---
+++ practice/src/reference/Point.java
@@ -0,0 +1,13 @@
+package reference;
+
+//Point - 점(좌표)을 추상화
+public class Point {
+	private int x;
+	private int y;
+	
+	public Point(int x, int y) {
+		this.x = x;
+		
+	}
+
+}
