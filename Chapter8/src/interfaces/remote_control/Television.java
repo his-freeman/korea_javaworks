bd99339dba5c9091cd59286fd21c1fa8c91f9ce4
his-freeman <ishan0414@naver.com>
2025-05-11 15:22:29 +0900

    interface

--- Chapter8/src/interfaces/remote_control/Television.java
+++ Chapter8/src/interfaces/remote_control/Television.java
@@ -20,7 +20,7 @@ public class Television implements RemoteControl{
 	public void setVolume(int volume) {
 		if(volume > RemoteControl.MAX_VOLUME)
 			this.volume = RemoteControl.MAX_VOLUME;
-		else if(volume > RemoteControl.MIN_VOLUME)
+		else if(volume < RemoteControl.MIN_VOLUME)
 			this.volume = RemoteControl.MIN_VOLUME;
 		else
 			this.volume = volume;
